8a255de700d890fa4f3a70ed9d79414425b5ecdb
Jeroen Laverman <jeroen@laverman.de>
2022-10-30 21:28:00 +0100

    fix removal compatiblity layer

--- example.py
+++ example.py
@@ -1,22 +1,19 @@
-from pysolarfocus import SolarfocusAPI, PORT,Systems
+from pysolarfocus import SolarfocusAPI ,Systems
 
 # Create the Solarfocus API client
 # TODO: Adapt IP-Address
-solarfocus = SolarfocusAPI(ip="10.10.10.237", heating_circuit_count=0, system=Systems.Vampair)
+solarfocus = SolarfocusAPI(ip="IP-Address", system=Systems.Vampair)
 solarfocus.connect()
 # Fetch the values
 solarfocus.update()
 
-
-
 # Print the values
-#print(solarfocus.heating_circuit)
-print("\n")
 print(solarfocus.heating_circuits[0])
 print("\n")
-#print(solarfocus.buffer)
-#print("\n")
-#print(solarfocus.heatpump)
-#print("\n")
-#print(solarfocus.photovoltaic)
-#
+print(solarfocus.boilers[0])
+print("\n")
+print(solarfocus.buffers[0])
+print("\n")
+print(solarfocus.heatpump)
+print("\n")
+print(solarfocus.photovoltaic)
