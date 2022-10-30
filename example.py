18051e8cd1c921113944d867091661c5ae6c4e61
Jeroen Laverman <jeroen@laverman.de>
2022-10-30 21:27:09 +0100

    Clean up and establish comaptibility

--- example.py
+++ example.py
@@ -2,18 +2,21 @@ from pysolarfocus import SolarfocusAPI, PORT,Systems
 
 # Create the Solarfocus API client
 # TODO: Adapt IP-Address
-solarfocus = SolarfocusAPI(ip="IP-Address", system=Systems.Vampair)
+solarfocus = SolarfocusAPI(ip="10.10.10.237", heating_circuit_count=0, system=Systems.Vampair)
 solarfocus.connect()
 # Fetch the values
 solarfocus.update()
 
+
+
 # Print the values
-print(solarfocus.heating_circuit)
+#print(solarfocus.heating_circuit)
 print("\n")
-print(solarfocus.boiler)
+print(solarfocus.heating_circuits[0])
 print("\n")
-print(solarfocus.buffer)
-print("\n")
-print(solarfocus.heatpump)
-print("\n")
-print(solarfocus.photovoltaic)
+#print(solarfocus.buffer)
+#print("\n")
+#print(solarfocus.heatpump)
+#print("\n")
+#print(solarfocus.photovoltaic)
+#
