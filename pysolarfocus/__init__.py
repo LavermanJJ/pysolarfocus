6f9cdbed4cde080a42c256a78a90c5fc7c1f78bd
lein1013 <lein1013@gmx.at>
2023-04-29 23:16:14 +0200

    add ecotop

--- pysolarfocus/__init__.py
+++ pysolarfocus/__init__.py
@@ -14,6 +14,7 @@ class Systems(str, Enum):
     """
     Vampair = "Vampair"
     Therminator = "Therminator" 
+    EcoTop = "EcoTop"
     
 class ApiVersions(str, Enum):
     """
--- pysolarfocus/components/pellets_boiler.py
+++ pysolarfocus/components/pellets_boiler.py
@@ -2,9 +2,10 @@ from .base.component import Component
 from .base.enums import DataTypes, RegisterTypes
 from .base.data_value import DataValue
 from .. import ApiVersions
+from .. import Systems
 
 class PelletsBoiler(Component):
-    def __init__(self,input_address=2400,holding_address=-1,api_version:ApiVersions=ApiVersions.V_21_140) -> None:
+    def __init__(self,input_address=2400,holding_address=-1,api_version:ApiVersions=ApiVersions.V_23_010,system=Systems.EcoTop) -> None:
         
         if api_version.greater_or_equal(ApiVersions.V_22_090.value):
             holding_address=33400
@@ -22,7 +23,7 @@ class PelletsBoiler(Component):
         self.octoplus_buffer_temperature_top = DataValue(address=11,multiplier=0.1)
         self.log_wood = DataValue(address=12,type=DataTypes.UINT)
         
-        if api_version.greater_or_equal(ApiVersions.V_22_090.value):
+        if api_version.greater_or_equal(ApiVersions.V_22_090.value) and  system is not Systems.EcoTop:
             self.sweep_function_start_stop = DataValue(address=10,register_type=RegisterTypes.Holding)
             self.sweep_function_extend = DataValue(address=11,register_type=RegisterTypes.Holding)
 
